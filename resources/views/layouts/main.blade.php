ba56b1b65c1f992644a1e14e9e74cffcb00c2bfc
r4mpo <erick1souza1ago04@gmail.com>
2022-08-13 18:02:01 -0300

    13-8 | 18:00 - Completed

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="/img/layout/icone.png.opdownload" type="image/x-icon">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     {{-- Bootstrap --}}
